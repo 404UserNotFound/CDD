e42b8099a335944f9aea94d0a08ba871a0b60f7a
404UserNotFound <mihaelabrodetchi@gmail.com>
2022-01-21 19:41:53 +0000

    Dining philosophers problem

--- LabFive/src/labfive/Main.java
+++ LabFive/src/labfive/Main.java
@@ -1,4 +1,10 @@
 package labfive;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Joe
@@ -7,14 +13,18 @@ package labfive;
  *  license: GNU General Public License v2.0
  */
 public class Main {
-    static final int MAX_T = 4;
+    static final int MAX_T = 8;
     public static void main(String[] args){
         /**IntegerObj total= new IntegerObj(0);*/
         // creates five tasks
-        Runnable r1 = new Task("task 1",total);
-        Runnable r2 = new Task("task 2",total);
-        Runnable r3 = new Task("task 3",total);
-        Runnable r4 = new Task("task 4",total);
+        Runnable r1 = new Leader("A",total);
+        Runnable r2 = new Leader("B",total);
+        Runnable r3 = new Leader("C",total);
+        Runnable r4 = new Leader("D",total);
+        Runnable r5 = new Follower("1",total);
+        Runnable r6 = new Follower("2",total);
+        Runnable r7 = new Follower("3",total);
+        Runnable r8 = new Follower("4",total);
 
         // creates a thread pool with MAX_T no. of
         // threads as the fixed pool size(Step 2)
@@ -25,14 +35,18 @@ public class Main {
         pool.execute(r2);
         pool.execute(r3);
         pool.execute(r4);
+        pool.execute(r5);
+        pool.execute(r6);
+        pool.execute(r7);
+        pool.execute(r8);
 
         // pool shutdown ( Step 4)
         pool.shutdown();
-        /**try {
+        try {
             Thread.sleep(2500);
         } catch (InterruptedException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
-        System.out.println("total is: "+total.value);*/
+        //System.out.println("total is: "+total.value);
     }
 }
---
+++ LabFive/src/labfive/Queue.java
@@ -0,0 +1,12 @@
+package labfive;
+
+public class Queue {
+    public static void dance(String s) {
+    }
+
+    public void followerWait(String followerName) {
+    }
+    public void leaderWait(){
+
+    }
+}
